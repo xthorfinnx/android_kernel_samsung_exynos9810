fbf3ea8f29775ef1a3c5433368129ac42f32feb6
Huang Jianan <huangjianan@oppo.com>
2021-12-01 22:54:36 +0800

    erofs: add sysfs interface
    Add sysfs interface to configure erofs related parameters later.
    
    Link: https://lore.kernel.org/r/20211201145436.4357-1-huangjianan@oppo.com
    Reviewed-by: Chao Yu <chao@kernel.org>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 168e9a76200c54c584a23aa88c62c53c4b0edd66)
    
    Conflicts:
    	fs/erofs/internal.h
    
    	Only the compr_head2 feature helper is kept, and the chunked_file/
    	device_table entries are dropped from the sysfs feature list, since
    	chunk-based files and multi-device support are not part of this
    	backport.
    
    Signed-off-by: Thorfinn <xxthorfinnxx@gmail.com>
    
    Conflicts:
    	Documentation/filesystems/erofs.rst
    	fs/erofs/internal.h

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -542,6 +542,10 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 	if (err)
 		return err;
 
+	err = erofs_register_sysfs(sb);
+	if (err)
+		return err;
+
 	erofs_info(sb, "mounted with root inode @ nid %llu.", ROOT_NID(sbi));
 	return 0;
 }
@@ -621,6 +625,7 @@ static void erofs_put_super(struct super_block *sb)
 
 	DBG_BUGON(!sbi);
 
+	erofs_unregister_sysfs(sb);
 	erofs_shrinker_unregister(sb);
 #ifdef CONFIG_EROFS_FS_ZIP
 	iput(sbi->managed_cache);
@@ -665,6 +670,10 @@ static int __init erofs_module_init(void)
 	if (err)
 		goto zip_err;
 
+	err = erofs_init_sysfs();
+	if (err)
+		goto sysfs_err;
+
 	err = register_filesystem(&erofs_fs_type);
 	if (err)
 		goto fs_err;
@@ -672,6 +681,8 @@ static int __init erofs_module_init(void)
 	return 0;
 
 fs_err:
+	erofs_exit_sysfs();
+sysfs_err:
 	z_erofs_exit_zip_subsystem();
 zip_err:
 	z_erofs_lzma_exit();
@@ -690,6 +701,7 @@ static void __exit erofs_module_exit(void)
 	/* Ensure all RCU free inodes / pclusters are safe to be destroyed. */
 	rcu_barrier();
 
+	erofs_exit_sysfs();
 	z_erofs_exit_zip_subsystem();
 	z_erofs_lzma_exit();
 	erofs_exit_shrinker();
