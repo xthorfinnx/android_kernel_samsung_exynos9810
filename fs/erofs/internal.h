0f08c467a322952360e3a721b24d19b11010221c
Huang Jianan <huangjianan@oppo.com>
2021-12-06 22:35:52 +0800

    erofs: add sysfs node to control sync decompression strategy
    Although readpage is a synchronous path, there will be no additional
    kworker scheduling overhead in non-atomic contexts together with
    dm-verity.
    
    Let's add a sysfs node to disable sync decompression as an option.
    
    Link: https://lore.kernel.org/r/20211206143552.8384-1-huangjianan@oppo.com
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 40452ffca3c1a0f2994e826f9fa213b107f1a2d4)
    
    Conflicts:
    	fs/erofs/internal.h
    
    	struct erofs_device_info (multi-device support, not backported) is
    	dropped; only the sync-decompression strategy enum is kept.
    
    Signed-off-by: Thorfinn <xxthorfinnxx@gmail.com>
    
    Conflicts:
    	fs/erofs/internal.h

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -48,12 +48,18 @@ typedef u64 erofs_off_t;
 /* data type for filesystem-wide blocks number */
 typedef u32 erofs_blk_t;
 
+enum {
+	EROFS_SYNC_DECOMPRESS_AUTO,
+	EROFS_SYNC_DECOMPRESS_FORCE_ON,
+	EROFS_SYNC_DECOMPRESS_FORCE_OFF
+};
+
 struct erofs_mount_opts {
 #ifdef CONFIG_EROFS_FS_ZIP
 	/* current strategy of how to use managed cache */
 	unsigned char cache_strategy;
-	/* strategy of sync decompression (false - auto, true - force on) */
-	bool readahead_sync_decompress;
+	/* strategy of sync decompression (0 - auto, 1 - force on, 2 - force off) */
+	unsigned int sync_decompress;
 
 	/* threshold for decompression synchronously */
 	unsigned int max_sync_decompress_pages;
