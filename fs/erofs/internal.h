dd1850c8a1493976f8b8080f5806f3d8d1f5f2ae
Jia Zhu <zhujia.zj@bytedance.com>
2022-09-18 12:34:53 +0800

    erofs: introduce fscache-based domain
    A new fscache-based shared domain mode is going to be introduced for
    erofs. In which case, same data blobs in same domain will be shared
    and reused to reduce on-disk space usage.
    
    The implementation of sharing blobs will be introduced in subsequent
    patches.
    
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
    Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20220918043456.147-4-zhujia.zj@bytedance.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 8b7adf1dff3d5baf687acda936f193f80b7e0179)

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -78,6 +78,7 @@ struct erofs_mount_opts {
 #endif
 	unsigned int mount_opt;
 	char *fsid;
+	char *domain_id;
 };
 
 struct erofs_dev_context {
@@ -98,6 +99,13 @@ struct erofs_sb_lz4_info {
 	u16 max_distance_pages;
 	/* maximum possible blocks for pclusters in the filesystem */
 	u16 max_pclusterblks;
+};
+
+struct erofs_domain {
+	refcount_t ref;
+	struct list_head list;
+	struct fscache_volume *volume;
+	char *domain_id;
 };
 
 struct erofs_fscache {
@@ -160,6 +168,7 @@ struct erofs_sb_info {
 	/* fscache support */
 	struct fscache_volume *volume;
 	struct erofs_fscache *s_fscache;
+	struct erofs_domain *domain;
 };
 
 #define EROFS_SB(sb) ((struct erofs_sb_info *)(sb)->s_fs_info)
