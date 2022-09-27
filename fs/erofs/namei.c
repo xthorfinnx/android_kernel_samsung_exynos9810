b8f10f4ff4421aec9c886ab67442b03bb696df1a
Gao Xiang <hsiangkao@linux.alibaba.com>
2022-09-27 14:36:06 +0800

    erofs: clean up unnecessary code and comments
    Some conditional macros and comments are useless.
    
    Link: https://lore.kernel.org/r/20220927063607.54832-1-hsiangkao@linux.alibaba.com
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 53a7f9961cddf505a9f3a7baa017e5af31838840)

--- fs/erofs/namei.c
+++ fs/erofs/namei.c
@@ -186,7 +186,6 @@ int erofs_namei(struct inode *dir,
 	if (IS_ERR(de))
 		return PTR_ERR(de);
 
-	/* the target page has been mapped */
 	if (ndirents)
 		de = find_target_dirent(&qn, (u8 *)de, EROFS_BLKSIZ, ndirents);
 
@@ -198,9 +197,7 @@ int erofs_namei(struct inode *dir,
 	return PTR_ERR_OR_ZERO(de);
 }
 
-/* NOTE: i_mutex is already held by vfs */
-static struct dentry *erofs_lookup(struct inode *dir,
-				   struct dentry *dentry,
+static struct dentry *erofs_lookup(struct inode *dir, struct dentry *dentry,
 				   unsigned int flags)
 {
 	int err;
@@ -208,17 +205,11 @@ static struct dentry *erofs_lookup(struct inode *dir,
 	unsigned int d_type;
 	struct inode *inode;
 
-	DBG_BUGON(!d_really_is_negative(dentry));
-	/* dentry must be unhashed in lookup, no need to worry about */
-	DBG_BUGON(!d_unhashed(dentry));
-
 	trace_erofs_lookup(dir, dentry, flags);
 
-	/* file name exceeds fs limit */
 	if (dentry->d_name.len > EROFS_NAME_LEN)
 		return ERR_PTR(-ENAMETOOLONG);
 
-	/* false uninitialized warnings on gcc 4.8.x */
 	err = erofs_namei(dir, &dentry->d_name, &nid, &d_type);
 
 	if (err == -ENOENT) {
