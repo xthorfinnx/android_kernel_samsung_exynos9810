72a5ff9a0f6a3fbecd14a14938c53619055bcc2a
Jia Zhu <zhujia.zj@bytedance.com>
2023-03-02 15:17:51 +0800

    erofs: support flattened block device for multi-blob images
    In order to support mounting multi-blobs container image as a single
    block device, add flattened block device feature for EROFS.
    
    In this mode, all meta/data contents will be mapped into one block
    space. User could compose a block device(by nbd/ublk/virtio-blk/
    vhost-user-blk) from multiple sources and mount the block device by
    EROFS directly. It can reduce the number of block devices used, and
    it's also benefits in both VM file passthrough and distributed storage
    scenarios.
    
    You can test this using the method mentioned by:
    https://github.com/dragonflyoss/image-service/pull/1139
    1. Compose a (nbd)block device from multi-blobs.
    2. Mount EROFS on mntdir/.
    3. Compare the md5sum between source dir and mntdir/.
    
    Later, we could also use it to refer original tar blobs.
    
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
    Signed-off-by: Xin Yin <yinxin.x@bytedance.com>
    Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Acked-by: Chao Yu <chao@kernel.org>
    Tested-by: Jiang Liu <gerry@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20230302071751.48425-1-zhujia.zj@bytedance.com
    [ Gao Xiang: refine commit message and use erofs_pos(). ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 8b465fecc35a434e61728a6184d188c6daa37a5d)

--- fs/erofs/data.c
+++ fs/erofs/data.c
@@ -200,7 +200,6 @@ int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *map)
 	struct erofs_device_info *dif;
 	int id;
 
-	/* primary device by default */
 	map->m_bdev = sb->s_bdev;
 	map->m_daxdev = EROFS_SB(sb)->dax_dev;
 	map->m_fscache = EROFS_SB(sb)->s_fscache;
@@ -212,11 +211,16 @@ int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *map)
 			up_read(&devs->rwsem);
 			return -ENODEV;
 		}
+		if (devs->flatdev) {
+			map->m_pa += erofs_pos(sb, dif->mapped_blkaddr);
+			up_read(&devs->rwsem);
+			return 0;
+		}
 		map->m_bdev = dif->bdev;
 		map->m_daxdev = dif->dax_dev;
 		map->m_fscache = dif->fscache;
 		up_read(&devs->rwsem);
-	} else if (devs->extra_devices) {
+	} else if (devs->extra_devices && !devs->flatdev) {
 		down_read(&devs->rwsem);
 		idr_for_each_entry(&devs->tree, dif, id) {
 			erofs_off_t startoff, length;
