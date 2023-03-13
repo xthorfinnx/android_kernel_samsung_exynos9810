e11824da84f6c8278eb965a0b5601268566f52f6
Jingbo Xu <jefflexu@linux.alibaba.com>
2023-03-13 21:53:09 +0800

    erofs: set block size to the on-disk block size
    Set the block size to that specified in on-disk superblock.
    
    Also remove the hard constraint of PAGE_SIZE block size for the
    uncompressed device backend.  This constraint is temporarily remained
    for compressed device and fscache backend, as there is more work needed
    to handle the condition where the block size is not equal to PAGE_SIZE.
    
    It is worth noting that the on-disk block size is read prior to
    erofs_superblock_csum_verify(), as the read block size is needed in the
    latter.
    
    Besides, later we are going to make erofs refer to tar data blobs (which
    is 512-byte aligned) for OCI containers, where the block size is 512
    bytes.  In this case, the 512-byte block size may not be adequate for a
    directory to contain enough dirents.  To fix this, we are also going to
    introduce directory block size independent on the block size.
    
    Due to we have already supported block size smaller than PAGE_SIZE now,
    disable all these images with such separated directory block size until
    we supported this feature later.
    
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Link: https://lore.kernel.org/r/20230313135309.75269-3-jefflexu@linux.alibaba.com
    [ Gao Xiang: update documentation. ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit d3c4bdcc756e60b95365c66ff58844ce75d1c8f8)
    
    Conflicts:
    	Documentation/filesystems/erofs.rst
    	fs/erofs/internal.h
    	fs/erofs/super.c

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -331,7 +331,6 @@ static int erofs_read_superblock(struct super_block *sb)
 	struct erofs_sb_info *sbi;
 	struct erofs_buf buf = __EROFS_BUF_INITIALIZER;
 	struct erofs_super_block *dsb;
-	unsigned int blkszbits;
 	void *data;
 	int ret;
 
@@ -350,6 +349,16 @@ static int erofs_read_superblock(struct super_block *sb)
 		goto out;
 	}
 
+	sbi->blkszbits  = dsb->blkszbits;
+	if (sbi->blkszbits < 9 || sbi->blkszbits > PAGE_SHIFT) {
+		erofs_err(sb, "blkszbits %u isn't supported", sbi->blkszbits);
+		goto out;
+	}
+	if (dsb->dirblkbits) {
+		erofs_err(sb, "dirblkbits %u isn't supported", dsb->dirblkbits);
+		goto out;
+	}
+
 	sbi->feature_compat = le32_to_cpu(dsb->feature_compat);
 	if (erofs_sb_has_sb_chksum(sbi)) {
 		ret = erofs_superblock_csum_verify(sb, data);
@@ -358,19 +367,11 @@ static int erofs_read_superblock(struct super_block *sb)
 	}
 
 	ret = -EINVAL;
-	blkszbits = dsb->blkszbits;
-	/* 9(512 bytes) + LOG_SECTORS_PER_BLOCK == LOG_BLOCK_SIZE */
-	if (blkszbits != LOG_BLOCK_SIZE) {
-		erofs_err(sb, "blkszbits %u isn't supported on this platform",
-			  blkszbits);
-		goto out;
-	}
-
 	if (!check_layout_compatibility(sb, dsb))
 		goto out;
 
 	sbi->sb_size = 128 + dsb->sb_extslots * EROFS_SB_EXTSLOT_SIZE;
-	if (sbi->sb_size > EROFS_BLKSIZ) {
+	if (sbi->sb_size > PAGE_SIZE - EROFS_SUPER_OFFSET) {
 		erofs_err(sb, "invalid sb_extslots %u (more than a fs block)",
 			  sbi->sb_size);
 		goto out;
@@ -705,8 +706,8 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 
 	sbi->blkszbits = PAGE_SHIFT;
 	if (erofs_is_fscache_mode(sb)) {
-		sb->s_blocksize = EROFS_BLKSIZ;
-		sb->s_blocksize_bits = LOG_BLOCK_SIZE;
+		sb->s_blocksize = PAGE_SIZE;
+		sb->s_blocksize_bits = PAGE_SHIFT;
 
 		err = erofs_fscache_register_fs(sb);
 		if (err)
@@ -715,8 +716,8 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 		/* fscache mode is not supported in this tree (no super_setup_bdi) */
 		return -EOPNOTSUPP;
 	} else {
-		if (!sb_set_blocksize(sb, EROFS_BLKSIZ)) {
-			erofs_err(sb, "failed to set erofs blksize");
+		if (!sb_set_blocksize(sb, PAGE_SIZE)) {
+			errorfc(fc, "failed to set initial blksize");
 			return -EINVAL;
 		}
 
@@ -730,6 +731,18 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 	sb->s_flags |= MS_RDONLY | MS_NOATIME;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sb->s_op = &erofs_sops;
+
+	if (sb->s_blocksize_bits != sbi->blkszbits) {
+		if (erofs_is_fscache_mode(sb)) {
+			errorfc(fc, "unsupported blksize for fscache mode");
+			return -EINVAL;
+		}
+		if (!sb_set_blocksize(sb, 1 << sbi->blkszbits)) {
+			errorfc(fc, "failed to set erofs blksize");
+			return -EINVAL;
+		}
+	}
+
 	sb->s_time_gran = 1;
 
 	sb->s_xattr = erofs_xattr_handlers;
