cf3878d7c0a5053b549567be680f0e5b84319a21
Jingbo Xu <jefflexu@linux.alibaba.com>
2023-03-13 21:53:08 +0800

    erofs: avoid hardcoded blocksize for subpage block support
    As the first step of converting hardcoded blocksize to that specified in
    on-disk superblock, convert all call sites of hardcoded blocksize to
    sb->s_blocksize except for:
    
    1) use sbi->blkszbits instead of sb->s_blocksize in
    erofs_superblock_csum_verify() since sb->s_blocksize has not been
    updated with the on-disk blocksize yet when the function is called.
    
    2) use inode->i_blkbits instead of sb->s_blocksize in erofs_bread(),
    since the inode operated on may be an anonymous inode in fscache mode.
    Currently the anonymous inode is allocated from an anonymous mount
    maintained in erofs, while in the near future we may allocate anonymous
    inodes from a generic API directly and thus have no access to the
    anonymous inode's i_sb.  Thus we keep the block size in i_blkbits for
    anonymous inodes in fscache mode.
    
    Be noted that this patch only gets rid of the hardcoded blocksize, in
    preparation for actually setting the on-disk block size in the following
    patch.  The hard limit of constraining the block size to PAGE_SIZE still
    exists until the next patch.
    
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Link: https://lore.kernel.org/r/20230313135309.75269-2-jefflexu@linux.alibaba.com
    [ Gao Xiang: fold a patch to fix incorrect truncated offsets. ]
    Link: https://lore.kernel.org/r/20230413035734.15457-1-zhujia.zj@bytedance.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 3acea5fc335420ba7ef53947cf2d98d07fac39f7)
    
    Conflicts:
    	fs/erofs/inode.c

--- fs/erofs/fscache.c
+++ fs/erofs/fscache.c
@@ -196,8 +196,8 @@ static int erofs_fscache_data_read(struct address_space *mapping,
 		void *src;
 
 		/* For tail packing layout, the offset may be non-zero. */
-		offset = erofs_blkoff(map.m_pa);
-		blknr = erofs_blknr(map.m_pa);
+		offset = erofs_blkoff(sb, map.m_pa);
+		blknr = erofs_blknr(sb, map.m_pa);
 		size = map.m_llen;
 
 		src = erofs_read_metabuf(&buf, sb, blknr, EROFS_KMAP);
@@ -448,6 +448,7 @@ static struct erofs_fscache *erofs_fscache_acquire_cookie(struct super_block *sb
 	inode->i_size = OFFSET_MAX;
 	inode->i_mapping->a_ops = &erofs_fscache_meta_aops;
 	mapping_set_gfp_mask(inode->i_mapping, GFP_NOFS);
+	inode->i_blkbits = EROFS_SB(sb)->blkszbits;
 	inode->i_private = ctx;
 
 	ctx->cookie = cookie;
