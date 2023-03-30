7a2837dfb359070fdb1cf327c0d7c6f3d98348a6
Jingbo Xu <jefflexu@linux.alibaba.com>
2023-03-30 16:29:04 +0800

    erofs: rename init_inode_xattrs with erofs_ prefix
    Rename init_inode_xattrs() to erofs_init_inode_xattrs() without logic
    change.
    
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20230330082910.125374-3-jefflexu@linux.alibaba.com
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Acked-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit 6020ffe76d1119241871300aad2ec7ef2cdc5e6c)

--- fs/erofs/xattr.c
+++ fs/erofs/xattr.c
@@ -16,7 +16,7 @@ struct xattr_iter {
 	unsigned int ofs;
 };
 
-static int init_inode_xattrs(struct inode *inode)
+static int erofs_init_inode_xattrs(struct inode *inode)
 {
 	struct erofs_inode *const vi = EROFS_I(inode);
 	struct xattr_iter it;
@@ -400,7 +400,7 @@ int erofs_getxattr(struct inode *inode, int index,
 	if (!name)
 		return -EINVAL;
 
-	ret = init_inode_xattrs(inode);
+	ret = erofs_init_inode_xattrs(inode);
 	if (ret)
 		return ret;
 
@@ -598,7 +598,7 @@ ssize_t erofs_listxattr(struct dentry *dentry,
 	int ret;
 	struct listxattr_iter it;
 
-	ret = init_inode_xattrs(d_inode(dentry));
+	ret = erofs_init_inode_xattrs(d_inode(dentry));
 	if (ret == -ENOATTR)
 		return 0;
 	if (ret)
