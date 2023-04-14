32a86f99aeab62f6ae7e696e61ad4a0542581a41
Gao Xiang <hsiangkao@linux.alibaba.com>
2023-04-14 16:30:27 +0800

    erofs: cleanup i_format-related stuffs
    Switch EROFS_I_{VERSION,DATALAYOUT}_BITS into
    EROFS_I_{VERSION,DATALAYOUT}_MASK.
    
    Also avoid erofs_bitrange() since its functionality is simple enough.
    
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Link: https://lore.kernel.org/r/20230414083027.12307-2-hsiangkao@linux.alibaba.com
    (cherry picked from commit 745ed7d77834048879bf24088c94e5a6462b613f)

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -312,24 +312,14 @@ struct erofs_inode {
 #define EROFS_I(ptr)	\
 	container_of(ptr, struct erofs_inode, vfs_inode)
 
-static inline unsigned int erofs_bitrange(unsigned int value, unsigned int bit,
-					  unsigned int bits)
+static inline unsigned int erofs_inode_version(unsigned int ifmt)
 {
-
-	return (value >> bit) & ((1 << bits) - 1);
+	return (ifmt >> EROFS_I_VERSION_BIT) & EROFS_I_VERSION_MASK;
 }
 
-
-static inline unsigned int erofs_inode_version(unsigned int value)
+static inline unsigned int erofs_inode_datalayout(unsigned int ifmt)
 {
-	return erofs_bitrange(value, EROFS_I_VERSION_BIT,
-			      EROFS_I_VERSION_BITS);
-}
-
-static inline unsigned int erofs_inode_datalayout(unsigned int value)
-{
-	return erofs_bitrange(value, EROFS_I_DATALAYOUT_BIT,
-			      EROFS_I_DATALAYOUT_BITS);
+	return (ifmt >> EROFS_I_DATALAYOUT_BIT) & EROFS_I_DATALAYOUT_MASK;
 }
 
 /*
