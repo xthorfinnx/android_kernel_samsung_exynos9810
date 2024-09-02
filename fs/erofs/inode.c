949643b8d88556e9b2cafeaa9a56dbe7b67cd7f4
Yiyang Wu <toolmanp@tlmp.cc>
2024-09-02 16:31:46 +0800

    erofs: use kmemdup_nul in erofs_fill_symlink
    Remove open coding in erofs_fill_symlink.
    
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Link: https://lore.kernel.org/all/20240425222847.GN2118490@ZenIV
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>
    Link: https://lore.kernel.org/r/20240902083147.450558-2-toolmanp@tlmp.cc
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    (cherry picked from commit b1bbb9a637a329873e14b596b7e6fa2fd44b87b4)
    
    Conflicts:
    	fs/erofs/inode.c

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -174,7 +174,6 @@ static int erofs_fill_symlink(struct inode *inode, void *kaddr,
 {
 	struct erofs_inode *vi = EROFS_I(inode);
 	unsigned int bsz = i_blocksize(inode);
-	char *lnk;
 
 	m_pofs += vi->xattr_isize;
 	/* check if it cannot be handled with fast symlink scheme */
@@ -184,14 +183,9 @@ static int erofs_fill_symlink(struct inode *inode, void *kaddr,
 		return 0;
 	}
 
-	lnk = kmalloc(inode->i_size + 1, GFP_KERNEL);
-	if (!lnk)
+	inode->i_link = kmemdup_nul(kaddr + m_pofs, inode->i_size, GFP_KERNEL);
+	if (!inode->i_link)
 		return -ENOMEM;
-
-	memcpy(lnk, kaddr + m_pofs, inode->i_size);
-	lnk[inode->i_size] = '\0';
-
-	inode->i_link = lnk;
 	inode->i_op = &erofs_fast_symlink_iops;
 	return 0;
 }
