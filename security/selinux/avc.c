8d3e98a8737c45cefdb2513a5d2fd4c44c5a0f0b
gavdov38 <gavin.a.docherty@gmail.com>
2025-08-16 23:29:03 +0100

    security: avc: susfs: Temp fix for avc log message being leaked by logd     - See full details:       https://android-review.googlesource.com/c/platform/system/logging/+/3725346/2       https://github.com/aviraxp/ZN-AuditPatch/blob/master/module/src/main/cpp/hook.cpp
        - So this is just a temp fix implemented in kernel side
    
        - Bonus: Add new toggle for enabling or disabling avc log spoofing

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -50,6 +50,13 @@
 #define avc_cache_stats_incr(field)	do {} while (0)
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+extern u32 susfs_ksu_sid;
+extern u32 susfs_kernel_sid;
+bool susfs_is_avc_log_spoofing_enabled = false;
+#endif
+
+
 struct avc_entry {
 	u32			ssid;
 	u32			tsid;
@@ -156,6 +163,17 @@ static void avc_dump_query(struct audit_buffer *ab, u32 ssid, u32 tsid, u16 tcla
 	u32 scontext_len;
 
 	rc = security_sid_to_context(ssid, &scontext, &scontext_len);
+
+#ifdef CONFIG_KSU_SUSFS
+	if (unlikely(tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
+		if (rc)
+			audit_log_format(ab, " tsid=%d", susfs_kernel_sid);
+		else
+			audit_log_format(ab, " tcontext=%s", "u:r:kernel:s0");
+		goto bypass_orig_flow;
+	}
+#endif
+
 	if (rc)
 		audit_log_format(ab, "ssid=%d", ssid);
 	else {
@@ -171,6 +189,9 @@ static void avc_dump_query(struct audit_buffer *ab, u32 ssid, u32 tsid, u16 tcla
 		kfree(scontext);
 	}
 
+#ifdef CONFIG_KSU_SUSFS
+bypass_orig_flow:
+#endif
 	BUG_ON(!tclass || tclass >= ARRAY_SIZE(secclass_map));
 	audit_log_format(ab, " tclass=%s", secclass_map[tclass-1].name);
 }
