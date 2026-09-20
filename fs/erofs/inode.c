fba2ad28baca91f8b7a18ea060473245a1c516e9
Thorfinn <xxthorfinnxx@gmail.com>
2026-09-20 15:30:23 +0500

    erofs: check the inline symlink size without check_add_overflow()
    4.9's helper is strictly typed.
    
    Signed-off-by: Thorfinn <xxthorfinnxx@gmail.com>

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -206,14 +206,12 @@ static int erofs_fill_symlink(struct inode *inode, void *kaddr,
 			      unsigned int m_pofs)
 {
 	struct erofs_inode *vi = EROFS_I(inode);
-	loff_t off;
 	char *lnk;
 
 	m_pofs += vi->xattr_isize;
 	/* check if it cannot be handled with fast symlink scheme */
 	if (vi->datalayout != EROFS_INODE_FLAT_INLINE || inode->i_size < 0 ||
-	    check_add_overflow(m_pofs, inode->i_size, &off) ||
-	    off > EROFS_BLKSIZ) {
+	    (u64)m_pofs + inode->i_size > EROFS_BLKSIZ) {
 		inode->i_op = &erofs_symlink_iops;
 		return 0;
 	}
