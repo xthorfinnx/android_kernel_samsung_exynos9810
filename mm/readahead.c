13b51b648f9dff74c6160e101a15c13daddcc3ac
Thorfinn <xxthorfinnxx@gmail.com>
2026-09-20 15:29:58 +0500

    mm/readahead: drop ra-state updates from readahead_expand()
    struct readahead_control has no ->ra here.
    
    Signed-off-by: Thorfinn <xxthorfinnxx@gmail.com>

--- mm/readahead.c
+++ mm/readahead.c
@@ -682,7 +682,6 @@ void readahead_expand(struct readahead_control *ractl,
 		      loff_t new_start, size_t new_len)
 {
 	struct address_space *mapping = ractl->mapping;
-	struct file_ra_state *ra = ractl->ra;
 	pgoff_t new_index, new_nr_pages;
 	gfp_t gfp_mask = readahead_gfp_mask(mapping);
 
@@ -727,10 +726,6 @@ void readahead_expand(struct readahead_control *ractl,
 			return;
 		}
 		ractl->_nr_pages++;
-		if (ra) {
-			ra->size++;
-			ra->async_size++;
-		}
 	}
 }
 EXPORT_SYMBOL(readahead_expand);
